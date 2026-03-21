f0c470b6b311947da6a5983cee68700b743f2c52
Degen Ape <degenapetrader@gmail.com>
2026-03-21 19:16:33 +0800

    Hide Windows console for desktop app

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,5 +1,6 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+// Prevents an extra console window when launching the packaged desktop app on
+// Windows. Keep this enabled for both debug and release bundles.
+#![cfg_attr(target_os = "windows", windows_subsystem = "windows")]
 
 fn main() {
     app_lib::run();
